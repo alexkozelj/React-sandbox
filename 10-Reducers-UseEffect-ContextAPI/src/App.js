f968661bb75a8862f635de968d042f0e945dfd19
aleksandarkozelj@gmail.com <aleksandarkozelj@gmail.com>
2021-10-21 21:46:23 +0200

    context api

--- 10-Reducers-UseEffect-ContextAPI/src/App.js
+++ 10-Reducers-UseEffect-ContextAPI/src/App.js
@@ -1,40 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-   const storedLoggedInInformation = localStorage.getItem('LoggedIn')
+   const storedLoggedInInformation = localStorage.getItem("LoggedIn");
 
    useEffect(() => {
-      if (storedLoggedInInformation === '1') {
+      if (storedLoggedInInformation === "1") {
          setIsLoggedIn(true);
       }
-   }, [])
+   }, []);
 
    const loginHandler = (email, password) => {
       // We should of course check email and password
       // But it's just a dummy/ demo anyways
-      localStorage.setItem('LoggedIn', '1');
+      localStorage.setItem("LoggedIn", "1");
       setIsLoggedIn(true);
    };
 
    const logoutHandler = () => {
-      localStorage.removeItem('LoggedIn');
+      localStorage.removeItem("LoggedIn");
       setIsLoggedIn(false);
    };
 
    return (
-      <React.Fragment>
-         <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <AuthContext.Provider 
+      value={{ 
+         isLoggedIn: isLoggedIn,
+         onLogout: logoutHandler,
+         }}>
+         <MainHeader/>
          <main>
             {!isLoggedIn && <Login onLogin={loginHandler} />}
             {isLoggedIn && <Home onLogout={logoutHandler} />}
          </main>
-      </React.Fragment>
+      </AuthContext.Provider>
    );
 }
 
