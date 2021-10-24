b57f576ece81a4fde4b532383576c09412eb1998
aleksandarkozelj@gmail.com <aleksandarkozelj@gmail.com>
2021-10-24 16:18:23 +0200

    added meal components

--- 11-Food-Order-App/src/App.js
+++ 11-Food-Order-App/src/App.js
@@ -1,9 +1,14 @@
+import { Fragment } from 'react'
+import Meals from './components/Meal/Meals'
+import Header from './components/Layout/Header'
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+   return (
+      <Fragment>
+         <Header />
+         <Meals />
+      </Fragment>
+   );
 }
 
 export default App;
